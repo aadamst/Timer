67ccfdeaa1d1898c87e3010431e16533816044f0
Shwizzyboi <shwizzyboi@gmail.com>
2022-11-24 05:20:53 +0000

    Added timer functionality

--- ASSETS/JAVASCRIT/TimerScript.js
+++ ASSETS/JAVASCRIT/TimerScript.js
@@ -1,20 +1,23 @@
-//Global variables
+// Global variables
 const start_btn = document.getElementById('start');
 const reset_btn = document.getElementById('reset');
 const unit_value = document.querySelectorAll('.unit-value');
+const add_btns = document.querySelectorAll('.add-btns');
+const arrows = document.querySelectorAll('.arrow');
 
 let centiSeconds = 0;
 let interval = null;
 let isCounting = false;
 let isStopwatch = true;
+let centi_Check = null;
 
-var SWButton = document.getElementsByClassName('stopwatch')[0];
-var TButton = document.getElementsByClassName('timer')[0];
-var timerAttr = document.getElementsByClassName('T-timer')[0];
-var addBtns = document.getElementsByClassName('T-timer')[1];
-var SWAttr = document.getElementsByClassName('SW-timer')[0];
+var stopwatch_btn = document.querySelector('.stopwatch');
+var timer_btn = document.querySelector('.timer');
+var timer_clock = document.querySelectorAll('.T-timer');
+var stopwatch_clock = document.querySelector('.SW-timer');
 var types = document.querySelectorAll('.type button');
 
+// Adds 'current' class to clicked button
 types.forEach(button => {
     button.addEventListener('click', () => {
         resetButtons();
@@ -22,30 +25,53 @@ types.forEach(button => {
     });
 });
 
+// Removes 'current' class from all buttons
 function resetButtons() {
     types.forEach(button => {
         button.classList.remove('current');
     });
 };
 
-//Event listeners
+// Event listeners
 start_btn.addEventListener('click', startStop);
 reset_btn.addEventListener('click', reset);
 
-SWButton.addEventListener('click', function(){
-    timerAttr.style.display = ('none');
-    addBtns.style.display = ('none');
-    SWAttr.style.display = ('flex');
+// Set clock to stopwatch mode
+stopwatch_btn.addEventListener('click', function(){
+
+    timer_clock.forEach(Ttimer => {
+        Ttimer.style.display = ('none');
+    });
+
+    stopwatch_clock.style.display = ('flex');
     isStopwatch = true;
+    clearInterval(centi_Check);
+    centi_Check = null;
+
+    stopwatch_btn.setAttribute('disabled', true);
+    timer_btn.removeAttribute('disabled');
+
+    enable();
+
     reset();
 });
 
-TButton.addEventListener('click', function(){
-    timerAttr.style.display = ('flex');
-    addBtns.style.display = ('flex');
-    SWAttr.style.display = ('none');
-    isStopwatch = false;
+// Set clock to timer mode
+timer_btn.addEventListener('click', function(){
+
+    timer_clock.forEach(Ttimer => {
+        Ttimer.style.display = ('flex');
+    });
+
+    stopwatch_clock.style.display = ('none');
+    isStopwatch = false
+    centi_Check = setInterval(centiCheck, 10);
+
+    timer_btn.setAttribute('disabled', true);
+    stopwatch_btn.removeAttribute('disabled');
+
     reset();
+    
 });
 
 document.querySelectorAll('input[type="Number"]').forEach(input =>{
@@ -55,14 +81,41 @@ document.querySelectorAll('input[type="Number"]').forEach(input =>{
 });
 
 
-//Update the stopwatch
+// Update the stopwatch
 function stopwatch(){
+    isCounting = true;
+    start_btn.classList.add('stop');
+    start_btn.innerText = 'stop';
     centiSeconds++;
+    timerCalc();
+}
 
-    //Format the time
-    let hrs = Math.floor(centiSeconds / 360000);
-    let mins = Math.floor((centiSeconds - (hrs * 360000)) / 6000);
-    let secs = Math.floor((centiSeconds - (Math.floor(centiSeconds / 6000) * 6000)) / 100);
+// Update the timer
+function timer(){
+    if(centiSeconds <= 0){
+        stop();
+        // Adds flash animation to unit values when timer hits 0
+        addAnim();
+    }
+    else {
+        // timer is countdown
+        isCounting = true;
+        start_btn.classList.add('pause');
+        start_btn.innerText = 'pause';
+        centiSeconds--;
+        timerCalc();
+    }
+}
+
+// Calculate timer
+function timerCalc(){
+    // Format the time
+    let hrsCalc = (centiSeconds / 360000);
+    let hrs = Math.floor(hrsCalc);
+    let minsCalc = (hrsCalc - hrs) * 60;
+    let mins = Math.floor(minsCalc);
+    let secsCalc = (minsCalc - mins) * 60;
+    let secs = Math.floor(secsCalc);
     let centi = centiSeconds % 100;
 
     if (centi < 10) centi = '0' + centi;
@@ -74,6 +127,19 @@ function stopwatch(){
     unit_value[1].innerText = `${mins}`;
     unit_value[2].innerText = `${secs}`;
     unit_value[3].innerText = `${centi}`;
+    unit_value[4].innerText = `${hrs}`;
+    unit_value[5].innerText = `${mins}`;
+    unit_value[6].innerText = `${secs}`;
+}
+
+// Checking centisecond value
+function centiCheck(){
+    if (centiSeconds >= 1){
+        enable();
+    } 
+    else {
+        disable();
+    }
 }
 
 // Start/Stop stopwatch/timer
@@ -86,29 +152,148 @@ function startStop(){
     } 
 }
 
+// start counting
 function start(){
-    isCounting = true;
-    start_btn.classList.add('stop');
-    start_btn.innerText = 'stop';
-    interval = setInterval(stopwatch, 10);
+    if (isStopwatch === true){
+        interval = setInterval(stopwatch, 10);
+    }
+    else {
+        interval = setInterval(timer, 10);
+    }
 }
 
+// stop counting
 function stop(){
     clearInterval(interval);
     isCounting = false;
-    start_btn.classList.remove('stop');
+    start_btn.classList.remove('stop', 'pause');
     start_btn.innerText = 'start';
     interval = null;
 }
 
+// Reset everything
 function reset(){
+    console.clear();
     stop();
     centiSeconds = 0;
-    unit_value[0].innerText = '00';
-    unit_value[1].innerText = '00';
-    unit_value[2].innerText = '00';
-    unit_value[3].innerText = '00';
-    unit_value[4].innerText = '00';
-    unit_value[5].innerText = '00';
-    unit_value[6].innerText = '00';
+
+    // Reset all unit values to '00'
+    unit_value.forEach(unitValue => {
+        unitValue.innerText = '00';
+    })
+
+    // Remove flash animation
+    removeAnim();
 }
+
+// Enables buttons
+function enable(){
+    start_btn.classList.remove('disable');
+    start_btn.removeAttribute('disabled'); 
+}
+
+// Disables buttons
+function disable(){
+    start_btn.classList.add('disable');
+    start_btn.setAttribute('disabled', true);
+}
+
+// Adds flash animation to unit values
+function addAnim(){
+    unit_value.forEach(unitValue => {
+        unitValue.classList.add('flash')
+    })
+}
+
+// Remove flash animation from unit values
+function removeAnim(){
+    unit_value.forEach(unitValue => {
+        unitValue.classList.remove('flash')
+    })
+}
+
+// Add time to timer
+// Add btns
+add_btns[0].addEventListener('click', function(){
+    // Adds  10mins to timer
+    centiSeconds += 60010;
+    timerCalc();
+    removeAnim();
+}); 
+
+add_btns[1].addEventListener('click', function(){
+    // Adds  5mins to timer
+    centiSeconds += 30005;
+    timerCalc();
+    removeAnim();
+}); 
+
+add_btns[2].addEventListener('click', function(){
+    // Adds  30secs to timer
+    centiSeconds += 3000;
+    timerCalc();
+    removeAnim();
+}); 
+
+add_btns[3].addEventListener('click', function(){
+    // Adds  10secs to timer
+    centiSeconds += 1000;
+    timerCalc();
+    removeAnim();
+}); 
+
+
+// Arrows
+arrows[0].addEventListener('click', function(){
+    // Adds 1hour to the timer
+    centiSeconds += 360000;
+    timerCalc();
+    removeAnim();
+});
+
+arrows[1].addEventListener('click', function(){
+    // Take away an hour (if there is enough time);
+    if(unit_value[4].innerText <= 0){
+        console.log('no time to take away');
+    }
+    else{
+        centiSeconds -= 360000;
+        timerCalc();
+    } 
+});
+
+arrows[2].addEventListener('click', function(){
+    // Adds 1min to the timer
+    centiSeconds += 6001;
+    timerCalc();
+    removeAnim();
+});
+
+arrows[3].addEventListener('click', function(){
+    // Take away 1min (if there is enough time);
+    if(unit_value[5].innerText <= 0){
+        console.log('no time to take away');
+    }
+    else{
+        centiSeconds -= 6001;
+        timerCalc();
+    } 
+});
+
+arrows[4].addEventListener('click', function(){
+    // Adds 1sec to the timer
+    centiSeconds += 100;
+    timerCalc();
+    removeAnim();
+});
+
+arrows[5].addEventListener('click', function(){
+    // Take away 1sec (if there is enough time);
+    if(unit_value[6].innerText <= 0){
+        console.log('no time to take away');
+    }
+    else{
+        centiSeconds -= 100;
+        timerCalc();
+    } 
+});
